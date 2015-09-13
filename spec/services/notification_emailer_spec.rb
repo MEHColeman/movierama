da98bdd7ec6a2a58c422e720da3173b3da4941cd
Mark Coleman <m@rkcoleman.co.uk>
2015-09-13 21:06:28 +0100

    Move around spec setup code
    We'll use the same setup for other tests, so move this up one level, out
    of this describe block.

--- spec/services/notification_emailer_spec.rb
+++ spec/services/notification_emailer_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 RSpec.describe NotificationEmailer do
 
+  let(:movie_submitter) {
+    double('User',
+           name: 'Testy McTesterson',
+           email: 'testy@example.com') }
+  let(:movie) {
+    double('Movie',
+           title: 'Star Wars: The Force Awakens',
+           user:  movie_submitter)
+  }
+  let(:voter) { double('User', name: 'Votey McVoter') }
+  let(:opinion) { :like }
+
+  subject {
+    NotificationEmailer.new(voter: voter, movie: movie, opinion: opinion)
+  }
+
   describe '.initialize' do
-    let(:movie_submitter) { double('User', name: 'Testy McTesterson') }
-    let(:movie) {
-      double('Movie',
-             title: 'Star Wars: The Force Awakens',
-             user:  movie_submitter)
-    }
-    let(:voter) { double('User', name: 'Votey McVoter') }
-    let(:opinion) { :like }
-
-    subject {
-      NotificationEmailer.new(user: voter, movie: movie, opinion: opinion)
-    }
-
     it 'takes a movie, a user and an opinion' do
       expect(subject).to be_a NotificationEmailer
     end
