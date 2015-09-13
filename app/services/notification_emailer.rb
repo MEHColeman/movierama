40c5a786293e99e7dd3e8973a41ddca9fd4e609d
Mark Coleman <m@rkcoleman.co.uk>
2015-09-13 21:00:43 +0100

    Rename parameters for clarity

--- app/services/notification_emailer.rb
+++ app/services/notification_emailer.rb
@@ -1,7 +1,8 @@
 # Used to send/queue up an email to the owner of a movie, telling that someone
 # expressed their opinion on one of their submissions.
 class NotificationEmailer
-  def initialize(movie:, user:, opinion:)
-    @movie, @user, @opinion = movie, user, opinion
+  def initialize(voter:, movie:, opinion:)
+    @voter, @movie, @opinion = voter, movie, opinion
+  end
   end
 end
